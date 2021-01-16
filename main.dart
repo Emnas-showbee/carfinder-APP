4215365e73b0784c75848c5aaafff84066c70056
Emnas_showbee <60103341+Emnas-showbee@users.noreply.github.com>
2021-01-16 15:39:05 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: "Park Finder"),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return WelcomePage();
+  }
+}
